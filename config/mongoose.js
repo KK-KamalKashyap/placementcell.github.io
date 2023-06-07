620997ba0fbd7f436cdf94c45625a9bc81b95b6d
KK-KamalKashyap <115741008+KK-KamalKashyap@users.noreply.github.com>
2023-06-07 12:12:01 +0530

    no commit

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+// requiring the library
+const mongoose = require("mongoose");
+
+//connecting to the database
+mongoose.connect(
+  "mongodb+srv://Nitish:nitish33@cluster0.ksobj.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+  // "mongodb+srv://placed:vishwa40@cluster0.k4ug4kl.mongodb.net/Student_data?retryWrites=true&w=majority"
+  // "mongodb+srv://Vishwajit:1pUFSRGhfU3tWOMC@cluster1.ftgiqi0.mongodb.net/Student_data?retryWrites=true&w=majority"
+  );
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+// checking for the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+// up and running then print the statement
+db.once("open", () => {
+  console.log("succesfully connected to database");
+});
+
+// exporting the connection
+module.exports = db;
